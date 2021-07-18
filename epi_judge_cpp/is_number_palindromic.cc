af1bb4b627ab02f93144ee4e971e770c4435bde2
kuldeep <kuldeep.yadav@noosyntech.in>
2021-07-18 15:07:45 +0530

    Practice the number palindromatic problem

--- epi_judge_cpp/is_number_palindromic.cc
+++ epi_judge_cpp/is_number_palindromic.cc
@@ -1,6 +1,20 @@
 #include "test_framework/generic_test.h"
+
 bool IsPalindromeNumber(int x) {
-  // TODO - you fill in here.
+  if(x < 0) return false;
+
+  const int n = static_cast<int>(floor(std::log10(x))) + 1;
+  int ms_mask = static_cast<int>(std::pow(10, n-1));
+
+  while(x) {
+    int lsd = x % 10;
+    int msd = x / ms_mask;
+    if(lsd != msd) return false;
+
+    x %= ms_mask;
+    x /= 10;
+    ms_mask /= 100;
+  }
   return true;
 }
 
