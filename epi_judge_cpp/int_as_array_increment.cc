83041a710f8316c2f907a03913431d71cf2187fb
kuldeep <kuldeep.yadav@noosyntech.in>
2021-07-19 21:22:28 +0530

    Practice the int as array

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -3,8 +3,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+  ++A[A.size()-1];
+  int i = A.size() - 1;
+  while(i > 0) {
+    if(A[i] == 10) {
+      A[i] = 0;
+      A[i-1]++;
+    }
+    i--;
+  }
+
+  if(A[0] == 10) {
+    A[0] = 1;
+    A.push_back(0);
+  }
+
+  return A;
 }
 
 int main(int argc, char* argv[]) {
