b427f721b52261863f6b0137d17223c25c3f1f27
kuldeep <kuldeep.yadav@noosyntech.in>
2021-07-25 22:02:59 +0530

    Buy and sell twice

--- epi_judge_cpp/buy_and_sell_stock_twice.cc
+++ epi_judge_cpp/buy_and_sell_stock_twice.cc
@@ -2,9 +2,29 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::numeric_limits;
+using std::min;
+using std::max;
+
 double BuyAndSellStockTwice(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+  double max_total_profit = 0.0;
+  
+  double min_price_so_far = numeric_limits<double>::max();
+  vector<double> profits(prices.size(), 0.0);
+
+  for(int i = 0; i < prices.size(); i++) {
+    min_price_so_far = min(min_price_so_far, prices[i]);
+    max_total_profit = max(max_total_profit, prices[i] - min_price_so_far);
+    profits[i] = max_total_profit;
+  }
+
+  double max_price_so_far = numeric_limits<double>::min();
+  for(int i = prices.size() - 1; i >=0 ; i--) {
+    max_price_so_far = max(max_price_so_far, prices[i]);
+    max_total_profit = max(max_total_profit, max_price_so_far - prices[i] + profits[i]);
+  }
+
+  return max_total_profit;
 }
 
 int main(int argc, char* argv[]) {
