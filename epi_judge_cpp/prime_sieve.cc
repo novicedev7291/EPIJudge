c52c9d19af697eff70c88817d9e04dd989f0b484
kuldeep <kuldeep.yadav@noosyntech.in>
2021-07-26 19:47:58 +0530

    Sieve of Eratosness

--- epi_judge_cpp/prime_sieve.cc
+++ epi_judge_cpp/prime_sieve.cc
@@ -1,11 +1,27 @@
 #include <vector>
+#include <deque>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::deque;
+
+#define ll long long
+
 // Given n, return all primes up to and including n.
 vector<int> GeneratePrimes(int n) {
-  // TODO - you fill in here.
-  return {};
+  vector<int> primes;
+
+  deque<bool> possiblePrimes(n+1, true);
+
+  for(ll i = 2; i < n + 1; i++) {
+    if(possiblePrimes[i]) {
+      primes.push_back(i);
+
+      for(ll j = i * i;  j <= n; j += i) possiblePrimes[j] = false;
+    }
+  } 
+
+  return primes;
 }
 
 int main(int argc, char* argv[]) {
